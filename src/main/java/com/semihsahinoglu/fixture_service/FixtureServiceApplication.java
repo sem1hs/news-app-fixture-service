9578b8266251aaef855831e678ab4526fcbeebcb
Semih Şahinoğlu <semihshngl@gmail.com>
2026-01-17 21:26:44 +0300

    dosyalar eklendi

---
+++ src/main/java/com/semihsahinoglu/fixture_service/FixtureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.semihsahinoglu.fixture_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixtureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FixtureServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semihsahinoglu/fixture_service/FixtureServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semihsahinoglu.fixture_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixtureServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
